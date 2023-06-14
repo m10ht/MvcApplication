7ff8b380b0d91cc7bd36526aaf0772292c164c2d
KhanhDang <vankhanhytf@gmail.com>
2023-06-14 11:11:18 +0700

    Add Identity

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -4,7 +4,7 @@ using App.Models.Contacts;
 
 namespace App.Models
 {
-    public class AppDbContext : DbContext {
+    public class AppDbContext : IdentityDbContext<AppUser> {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
         
         }
@@ -18,11 +18,11 @@ namespace App.Models
         {
             base.OnModelCreating(modelBuilder);
             // bo tien to AspNet trong ten table khi tao table bang migrations
-            // foreach (var identityType in modelBuilder.Model.GetEntityTypes()) {
-            //     var tableName = identityType.GetTableName();
-            //     if (tableName.StartsWith("AspNet"))
-            //         identityType.SetTableName(tableName.Substring(6));
-            // }
+            foreach (var identityType in modelBuilder.Model.GetEntityTypes()) {
+                var tableName = identityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                    identityType.SetTableName(tableName.Substring(6));
+            }
 
         } 
         public DbSet<Contact> Contacts {get; set;}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using App.Models;
 using App.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Routing.Constraints;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,62 @@ builder.Services.AddRazorPages();       // them razor page
 
 builder.Services.AddSingleton<PlanetService>();
 builder.Services.AddSingleton<ProductService>();
+
+// Dang ky Identity
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+
+
+// Truy cập IdentityOptions
+builder.Services.Configure<IdentityOptions> (options => {
+    // Thiết lập về Password
+    options.Password.RequireDigit = false; // Không bắt phải có số
+    options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+    options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+    options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+    options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+    options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+    // Cấu hình Lockout - khóa user
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes (5); // Khóa 5 phút
+    options.Lockout.MaxFailedAccessAttempts = 3; // Thất bại 3 lần thì khóa
+    options.Lockout.AllowedForNewUsers = true;
+
+    // Cấu hình về User.
+    options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+    // Cấu hình đăng nhập.
+    options.SignIn.RequireConfirmedEmail = true;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+    options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+    options.SignIn.RequireConfirmedAccount = true;
+});
+// Authentication: xác thực danh tính -> Login, Logout
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = "/login/";
+    options.LogoutPath = "/logout/";
+    options.AccessDeniedPath = "/khongduoctruycap.html";
+});
+
+builder.Services.AddAuthentication()
+                .AddGoogle(googleOptions => {
+                    var googleConfig = builder.Configuration.GetSection("web:Google");
+                    googleOptions.ClientId = googleConfig["client_id"];
+                    googleOptions.ClientSecret = googleConfig["client_secret"];
+                    // https://localhost:7237/signin-google
+                    googleOptions.CallbackPath = "/dang-nhap-tu-google";
+                })
+                .AddFacebook(facebookOptions => {
+                    var facebookConfig = builder.Configuration.GetSection("web:Facebook");
+                    facebookOptions.AppId = facebookConfig["app_id"];
+                    facebookOptions.AppSecret = facebookConfig["app_secret"];
+                    facebookOptions.CallbackPath = "/dang-nhap-tu-facebook";
+                });
+
+
 
 builder.Services.Configure<RazorViewEngineOptions>(options => {
     //đường dẫn mặc định: /View/Controller/Action.cshtml
